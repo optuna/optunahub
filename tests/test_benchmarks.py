40f1312728eb250032643616945f55984b7a357f
y0z <y0z@users.noreply.github.com>
2024-12-06 19:37:22 +0900

    Add test_benchmarks

---
+++ tests/test_benchmarks.py
@@ -0,0 +1,26 @@
+import optuna
+
+import optunahub
+
+
+def test_base_problem() -> None:
+    class TestProblem(optunahub.benchmarks.BaseProblem):
+        def evaluate(self, params: dict[str, float]) -> float:
+            x = params["x"]
+            return x**2
+
+        @property
+        def search_space(self) -> dict[str, optuna.distributions.BaseDistribution]:
+            return {"x": optuna.distributions.FloatDistribution(low=-1, high=1)}
+
+        @property
+        def directions(self) -> list[optuna.study.StudyDirection]:
+            return [optuna.study.StudyDirection.MINIMIZE]
+
+    problem = TestProblem()
+    assert problem.search_space == {"x": optuna.distributions.FloatDistribution(low=-1, high=1)}
+    assert problem.directions == [optuna.study.StudyDirection.MINIMIZE]
+    assert problem.evaluate({"x": 0.0}) == 0.0
+
+    study = optuna.create_study()
+    study.optimize(problem, n_trials=20)  # verify no error occurs
