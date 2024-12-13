61b44f631ed69b0072f9639523552282d6386b4e
y0z <y0z@users.noreply.github.com>
2024-12-13 15:50:57 +0900

    Add test

--- tests/test_benchmarks.py
+++ tests/test_benchmarks.py
@@ -5,20 +5,36 @@ import optuna
 import optunahub
 
 
+class TestProblem(optunahub.benchmarks.BaseProblem):
+    def evaluate(self, params: dict[str, float]) -> float:
+        x = params["x"]
+        return x**2
+
+    @property
+    def search_space(self) -> dict[str, optuna.distributions.BaseDistribution]:
+        return {"x": optuna.distributions.FloatDistribution(low=-1, high=1)}
+
+    @property
+    def directions(self) -> list[optuna.study.StudyDirection]:
+        return [optuna.study.StudyDirection.MINIMIZE]
+
+
 def test_base_problem() -> None:
-    class TestProblem(optunahub.benchmarks.BaseProblem):
-        def evaluate(self, params: dict[str, float]) -> float:
-            x = params["x"]
-            return x**2
-
-        @property
-        def search_space(self) -> dict[str, optuna.distributions.BaseDistribution]:
-            return {"x": optuna.distributions.FloatDistribution(low=-1, high=1)}
-
-        @property
-        def directions(self) -> list[optuna.study.StudyDirection]:
-            return [optuna.study.StudyDirection.MINIMIZE]
-
     problem = TestProblem()
     study = optuna.create_study(directions=problem.directions)
     study.optimize(problem, n_trials=20)  # verify no error occurs
+
+
+def test_constrained_mixin() -> None:
+    class ConstrainedTestProblem(optunahub.benchmarks.ConstrainedMixIn, TestProblem):
+        def evaluate_constraints(self, params: dict[str, float]) -> list[float]:
+            return [params["x"]]
+
+    problem = ConstrainedTestProblem()
+    sampler = optuna.samplers.TPESampler(constraints_func=problem.constraints_func)
+    study = optuna.create_study(sampler=sampler, directions=problem.directions)
+    study.optimize(problem, n_trials=20)  # verify no error occurs
+
+    # Check if constraints are stored in trials
+    for t in study.trials:
+        assert "constraints" in t.system_attrs
