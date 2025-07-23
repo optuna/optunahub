18c067dde130696fa24444b0d68376473ae4a88b
fusawayugo <fusawa0712@gmail.com>
2025-07-23 17:26:34 +0900

    Add test

--- tests/test_hub.py
+++ tests/test_hub.py
@@ -2,13 +2,16 @@ from __future__ import annotations
 
 import os
 import shutil
+import time
 
 import optuna
 import pytest
 from pytest import MonkeyPatch
 
 import optunahub
+from optunahub import _conf
 from optunahub.hub import _extract_hostname
+from optunahub.hub import _is_cache_valid
 
 
 @pytest.mark.parametrize("git_command", ["/usr/bin/git", None])
@@ -76,3 +79,45 @@ def test_if_report_stats_is_called(monkeypatch: MonkeyPatch, git_command: str | 
     monkeypatch.setattr("optunahub.hub._report_stats", lambda *args, **kwargs: calls.append(None))
     optunahub.load_module(package="dummy/test")
     assert len(calls) > 0
+
+
+@pytest.mark.parametrize(
+    "mtime_offset, cache_expiration_seconds, expected",
+    [
+        (-10, 30, True),
+        (-40, 30, False),
+        (-30, 30, False),
+    ],
+)
+def test_is_cache_valid(
+    monkeypatch: MonkeyPatch, mtime_offset: int, cache_expiration_seconds: int, expected: bool
+) -> None:
+    registry_root = "package"
+    package = "samplers/auto_sampler"
+    repo_owner = "optuna"
+    repo_name = "optunahub-registry"
+    ref = "main"
+    base_url = "git@github.com"
+    hostname = _extract_hostname(base_url)
+    assert hostname is not None
+    cache_dir_prefix = os.path.join(_conf.cache_home(), hostname, repo_owner, repo_name, ref)
+    package_cache_dir = os.path.join(cache_dir_prefix, registry_root, package)
+
+    _ = optunahub.load_module(
+        package,
+        force_reload=True,
+        repo_owner=repo_owner,
+        repo_name=repo_name,
+        ref=ref,
+        base_url=base_url,
+    )
+
+    monkeypatch.setenv("OPTUNAHUB_CACHE_EXPIRATION_SECONDS", str(cache_expiration_seconds))
+
+    fake_now = 1000000000
+    monkeypatch.setattr(time, "time", lambda: fake_now)
+    modified_time = fake_now + mtime_offset
+    monkeypatch.setattr(
+        "optunahub.hub._get_cache_latest_modified_time", lambda package_cache_dir: modified_time
+    )
+    assert _is_cache_valid(package_cache_dir) is expected
